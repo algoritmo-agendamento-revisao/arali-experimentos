8c6d60c7d29600988636cdb48c7de0caf52a8c57
joaomarcelofa <joaov7.1996@gmail.com>
2018-09-29 05:21:14 -0300

    Adicionado parâmetro para imprimir ou não o card - Alteração da forma de cálculo da próxima repetição (calculado intervalo em dias de acordo com a repetição atual e a repetição anterior)

---
+++ modelos/mapeador_estado_acao/utilitarioqlearning.py
@@ -0,0 +1,31 @@
+from tabulate import tabulate
+import numpy as np
+
+class UtilitarioQLearning:
+
+    def __init__(self):
+        pass
+
+    def mapear_numero_repeticao_em_estado(self, repeticao: int):
+        # tirado 2, pois tratamos de atualizar a tabela para ação de determinar o ef passado e - 1 do indice do array
+        # EX: repetição 2 - Recompensa do EF 1, tira-se 1 por causa do índice do array, então ficaria linha 0 na tabela
+        return repeticao - 2
+
+    def mapear_estado_em_numero_de_repeticao(self):
+        pass
+
+    def mapear_ef_em_acao(self, ef):
+        # Mapeia ef de 1.3 a 2.8 em 0 até 15 #TODO:verificar se vai até 15 mesmo
+        return int((ef - 1.3) * 10)
+
+    def mapear_acao_em_ef(self, acao):
+        #Mapeia acao de 0 a 15 em ef de 1.3 a 2.8
+        return acao * 0.1 + 1.3
+
+    def imprimir_tabela_formatada(self, tabela):
+        coluna_indice = np.array([[1],[2],[3],[4],[5],[6],[7],[8],[9]])
+        nova_tabela = np.append(coluna_indice, tabela[:,1:], axis=1)
+        headers = ['1.3', '1.4', '1.5', '1.6', '1.7', '1.8', '1.9',
+                   '2.0', '2.1', '2.2', '2.3', '2.4', '2.5', '2.6', '2.7']
+        return nova_tabela
+        #print(tabulate(nova_tabela, headers=headers, tablefmt='orgtbl'))
--- reinforcement_learning_implementation.py
+++ reinforcement_learning_implementation.py
@@ -1,9 +1,7 @@
 # Libraries
 import numpy as np
-import pandas as pd
 from datetime import datetime
 from datetime import timedelta
-from tabulate import tabulate
 import random
 
 from modelos.cards.card import Card
@@ -11,13 +9,16 @@ from modelos.resposta_usuario.respostausuario import RespostaUsuario
 from modelos.estudo.estudo import Estudo
 from modelos.cards.gerenciadorcard import GerenciadorCard
 from modelos.estudo.gerenciadorestudo import GerenciadorEstudo
+from modelos.mapeador_estado_acao.utilitarioqlearning import UtilitarioQLearning
 
 
 class Agente:
     __qtd_possiveis_estados__ = 8
-    __qtd_possiveis_efs__ = 10
-    lr = .8
-    y = .95
+    __qtd_possiveis_efs__ = 16
+    __utilitario_qlearning__ = UtilitarioQLearning()
+
+    lr = .8 #taxa de aprendizagem
+    y = .10 #fator de desconto
 
     __gerenciador_card__: GerenciadorCard = GerenciadorCard()
     __gerenciador_estudo__: GerenciadorEstudo = GerenciadorEstudo()
@@ -33,17 +34,16 @@ class Agente:
         return timedelta(days=oi.days)
 
     def __inicializar_tabela_qlearning__(self):
-        self.q_table = np.zeros([self.__qtd_possiveis_estados__, self.__qtd_possiveis_efs__])
+        self.q_table = np.zeros([self.__qtd_possiveis_estados__ + 1, self.__qtd_possiveis_efs__])
 
     # Atualiza tabela/s do Q-learning
     def atualizar_politica(self, recompensa, estudo: Estudo):
-        # tirado 2, pois tratamos de atualizar a tabela para ação de determinar o ef passado e - 1 do indice do array
-        # EX: repetição 2 - Recompensa do EF 1, tira-se 1 por causa do índice do array, então ficaria linha 0 na tabela
-        ultima_repeticao = estudo.numero_repeticao - 2
-        estado = ultima_repeticao
-        card_ef = int( (self.__gerenciador_card__.buscar_card(estudo.card_id).ef - 1.3) * 10)
+        estado = self.__utilitario_qlearning__.mapear_numero_repeticao_em_estado(estudo.numero_repeticao)
 
-        #TODO: Arrumar o ultimo estado_bugando do np.max
+        card_ef = self.__gerenciador_card__.buscar_card(estudo.card_id).ef
+        card_ef = self.__utilitario_qlearning__.mapear_ef_em_acao(card_ef)
+
+        #TODO: Trocar a fórmula do q-learning pra uma de recompensa não determinística
         self.q_table[estado, card_ef] = self.q_table[estado, card_ef] + \
                                             self.lr * (
                                                     recompensa +
@@ -61,25 +61,33 @@ class Agente:
         card = self.__gerenciador_card__.buscar_card(estudo.card_id)
         #atualizar com o Q-learning - Exloration vs Explotation
         estado = estudo.numero_repeticao - 1
-        coluna_do_ef = np.argmax(
+        acao = np.argmax(
             self.q_table[estado, :] +
             np.random.randn(1, self.__qtd_possiveis_efs__) * (1. / (estado + 1))
         )
-        card.ef = coluna_do_ef * 0.1 + 1.3
+        #mapeando o resultado do ef na coluna da tabela
+        card.ef = self.__utilitario_qlearning__.mapear_acao_em_ef(acao)
         self.__gerenciador_card__.atualizar_card(card)
 
     def __calcular_proxima_repeticao__(self, estudo: Estudo):
         card_corrente: Card = self.__gerenciador_card__.buscar_card(estudo.card_id)
+        ef = card_corrente.ef
         if estudo.numero_repeticao is 1:
             estudo.data_ultima_repeticao = estudo.data_primeira_repeticao
+            estudo.data_proxima_repeticao = estudo.data_primeira_repeticao + self.__calcular_oi__(ef, estudo.numero_repeticao)
         else:
             # Simulando que o tempo passou o tempo e que o card está sendo exibido na data estipulado
             estudo.data_ultima_repeticao = estudo.data_proxima_repeticao  # datetime.now()
+            estudo.data_proxima_repeticao += self.__calcular_intervalo_em_dias__(ef, estudo.numero_repeticao)
 
-        estudo.data_proxima_repeticao = estudo.data_primeira_repeticao + self.__calcular_oi__(card_corrente.ef,
-                                                                                              estudo.numero_repeticao)
         estudo.numero_repeticao += 1
         return estudo
+
+    def __calcular_intervalo_em_dias__(self, ef, repeticao):
+        return (self.__calcular_oi__(ef, repeticao) - self.__calcular_oi__(ef, repeticao-1))
+
+    def imprimir_tabela(self):
+        return self.__utilitario_qlearning__.imprimir_tabela_formatada(self.q_table)
 
 
 # Ambiente
@@ -87,11 +95,12 @@ class UserInterface:
     __tempo_inicial__: datetime
     __opcao_correta__: str
 
-    def mostrar_card(self, card: Card):
-        print("Digite o número ao lado da qustão para selecionar a resposta")
-        print(f'Questão: {card.questao}')
-        for index, questao in enumerate(card.opcoes):
-            print(f'{index} - {questao}')
+    def mostrar_card(self, card: Card, imprimir_questao = True):
+        if imprimir_questao:
+            print("Digite o número ao lado da qustão para selecionar a resposta")
+            print(f'Questão: {card.questao}')
+            for index, questao in enumerate(card.opcoes):
+                print(f'{index} - {questao}')
 
         self.__tempo_inicial__ = datetime.now()
         self.__opcao_correta__ = card.opcoes.index(card.opcao_correta)
@@ -122,6 +131,13 @@ class UserInterface:
         print('---------------------------------------------------------------------------------------')
         return resposta_usuario
 
+    def obter_resposta_automatica(self, tempo_resposta_em_millis: int = None, acerto: bool = None):
+        if tempo_resposta_em_millis is None:
+            tempo_resposta_em_millis = random.randint(0, 20000)
+        if acerto is None:
+            acerto = True #if (random.randint(0, 1) == 1) else False
+        return RespostaUsuario(tempo_resposta_em_millis, acerto)
+
     def calcular_recompensa(self, resposta: RespostaUsuario, estudo_corrente: Estudo):
         intervalo = (estudo_corrente.data_proxima_repeticao - estudo_corrente.data_ultima_repeticao).days
         repeticao = estudo_corrente.numero_repeticao
@@ -139,11 +155,11 @@ class Controlador:
 
     def run(self):
         # Inicializando os componentes do algoritmo
-        estudante = Estudante()
         agente = Agente()
         user_interface = UserInterface()
         gerenciador_card = GerenciadorCard()
         gerenciador_estudo = GerenciadorEstudo()
+        tabelas = []
 
         agente.__inicializar_tabela_qlearning__()
 
@@ -153,24 +169,24 @@ class Controlador:
         for card in cards:
             gerenciador_estudo.criar_estudo(card)
 
-        for episodio in range(1, 9):
-            for card in cards:
-                user_interface.mostrar_card(card)
-                resposta_do_usuario = user_interface.obter_resposta()
+        for card in cards:
+            for episodio in range(1, 10):
+                user_interface.mostrar_card(card, imprimir_questao=False)
+                resposta_do_usuario = user_interface.obter_resposta_automatica()
                 estudo_corrente = gerenciador_estudo.buscar_estudo(card.id)
 
                 if estudo_corrente.numero_repeticao is not 1:
                     recompensa = user_interface.calcular_recompensa(resposta_do_usuario, estudo_corrente)
-                    print(f'recompensa {recompensa}')
+                    print(f'recompensa {recompensa} | '
+                          f'Acertou: {resposta_do_usuario.acerto} | '
+                          f'Tempo: {resposta_do_usuario.tempo_resposta}')
                     # novo_estado = user_interface.gerar_proximo_estado(card.tag, card.ef, estudo_corrente.numero_repeticao)
                     agente.atualizar_politica(recompensa, estudo_corrente)
 
                 # Agente toma ação / Fornece estado do ambiente
                 agente.atualizar_estudo(estudo_corrente)
-
-        gerenciador_estudo.imprimir_estudos()
-        table = tabulate(agente.q_table,[],tablefmt="fancy_grid")
-        print(table)
+                tabelas.append(agente.imprimir_tabela())
+        pass
 
 controlador = Controlador()
 controlador.run()
