697094d11ab65bc4513f7b844a53997a861c6291
João Marcelo <joaomarcelo@juit.io>
2018-11-20 22:52:34 -0200

    Adicionado try except para datas muito altas

--- modelos_matematicos/formula_repeticao/formula_repeticao.py
+++ modelos_matematicos/formula_repeticao/formula_repeticao.py
@@ -2,10 +2,13 @@ from datetime import timedelta
 import numpy as np
 
 def calcular_oi(ef, repeticao):
-    if repeticao is 1:
-        return timedelta(days=5)
+    try:
+        if repeticao is 1:
+            return timedelta(days=5)
 
-    part1 = calcular_oi(ef, repeticao - 1)
-    part2 = ef - 0.1 + np.power(np.e, (-2.3 * repeticao + 5))
-    oi = part1 * part2
-    return timedelta(days=oi.days)
+        part1 = calcular_oi(ef, repeticao - 1)
+        part2 = ef - 0.1 + np.power(np.e, (-2.3 * repeticao + 5))
+        oi = part1 * part2
+        return timedelta(days=oi.days)
+    except Exception:
+        return timedelta(days=10000)
