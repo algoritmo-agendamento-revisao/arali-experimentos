27b2f10171fac13b0592ea7cafb43df27703f22f
João Marcelo <joaomarcelo@juit.io>
2018-11-20 22:01:24 -0200

    Removido código não utilizado

--- user_interface/userinterface.py
+++ user_interface/userinterface.py
@@ -12,42 +12,6 @@ class UserInterface:
         self.__tempo_inicial__: datetime
         self.__opcao_correta__: str
 
-    def mostrar_card(self, card: Card, imprimir_questao=True):
-        if imprimir_questao:
-            print("Digite o número ao lado da qustão para selecionar a resposta")
-            print(f'Questão: {card.questao}')
-            for index, questao in enumerate(card.opcoes):
-                print(f'{index} - {questao}')
-
-        self.__tempo_inicial__ = datetime.now()
-        self.__opcao_correta__ = card.opcoes.index(card.opcao_correta)
-
-    def obter_resposta(self):
-        resposta_usuario = RespostaUsuario()
-
-        input_invalido = True
-        while input_invalido:
-            input_do_usuario = input("Selecione a questão:")
-            # Caso aperte um caractere indesejado o indice da questão é setado para 1
-            try:
-                indice_da_questao = int(input_do_usuario)
-            except:
-                indice_da_questao = 1
-
-            if indice_da_questao >= 0 and indice_da_questao <= 3:
-                input_invalido = False
-                tempo_final = datetime.now()
-                tempo_de_resposta_em_millis = int((tempo_final - self.__tempo_inicial__).total_seconds() * 1000)
-                resposta_usuario.tempo_resposta = tempo_de_resposta_em_millis
-
-                if indice_da_questao == self.__opcao_correta__:
-                    resposta_usuario.acerto = True
-                else:
-                    resposta_usuario.acerto = False
-
-        print('---------------------------------------------------------------------------------------')
-        return resposta_usuario
-
     def obter_resposta_automatica(self, tempo_resposta_em_millis: int=None, acerto: bool=None, intervalo:int=None):
         if tempo_resposta_em_millis is None:
             tempo_resposta_em_millis = distribuicao_normal(5, 1.5)
